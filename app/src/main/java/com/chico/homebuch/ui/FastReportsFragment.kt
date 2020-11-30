e1360788fdb6159391b08a514d6f5321849f0484
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-11-30 22:55:38 +0300

    bugs fixed

--- app/src/main/java/com/chico/homebuch/ui/FastReportsFragment.kt
+++ app/src/main/java/com/chico/homebuch/ui/FastReportsFragment.kt
@@ -1,10 +1,11 @@
-package com.chico.homebuch
+package com.chico.homebuch.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chico.homebuch.R
 
 class FastReportsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
